1f628d7b3f59c65d1ba8d91b26563e124cc555bd
Ryan Arnett <ryanglennarnett@gmail.com>
2017-05-22 14:14:08 -0500

    got a skeleton

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+console.log('ready!');
+
+ var config = {
+    apiKey: "AIzaSyCx2kt6qk1xCDmnWgJ7M-GfjKMSDCLGkVM",
+    authDomain: "rps-multiplayer-c433d.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-c433d.firebaseio.com",
+    projectId: "rps-multiplayer-c433d",
+    storageBucket: "rps-multiplayer-c433d.appspot.com",
+    messagingSenderId: "246513056808"
+  };
+  firebase.initializeApp(config);
+
+
+
+})
